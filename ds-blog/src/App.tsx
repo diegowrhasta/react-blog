70cc77385f12152ab24496292d18f70b382a2c85
Diego Balderrama <diegowrhasta@gmail.com>
2024-08-30 10:06:39 -0400

    Adds svgs for mode button on header, creates new component and pipeline to bubble up click on mode button all the way to App

--- ds-blog/src/App.tsx
+++ ds-blog/src/App.tsx
@@ -1,16 +1,23 @@
-import './App.css';
-import { Header } from './components/Header';
+import React from 'react'
 
-function App() {
+import { Header } from './components/Header'
+
+import './App.css'
+
+function App () {
+  function onChangeMode (event: React.MouseEvent<HTMLButtonElement>): void {
+    console.log('app', event)
+  }
+
   return (
     <>
-      <div className="container">
-        <Header></Header>
+      <div className='container'>
+        <Header onModeButtonClick={onChangeMode}></Header>
         <main>MAIN</main>
         <footer>FOOTER</footer>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- ds-blog/src/components/Header.tsx
+++ ds-blog/src/components/Header.tsx
@@ -1,20 +1,30 @@
-import './Header.css';
+import React from 'react'
 
-export function Header() {
+import { ModeButton } from './ModeButton'
+
+import './Header.css'
+
+interface HeaderProps {
+  onModeButtonClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export function Header ({ onModeButtonClick }: HeaderProps) {
+  function onModeClick (event: React.MouseEvent<HTMLButtonElement>) {
+    onModeButtonClick(event)
+  }
+
   return (
     <header>
-      <div className="header-container">
+      <div className='header-container'>
         <h3>Diego Balderrama</h3>
         <nav>
-          <a href="#recent">Recent</a>
-          <a href="#all">All</a>
-          <a href="#about">About</a>
-          <a href="#">Mode</a>
+          <a href='#recent'>Recent</a>
+          <a href='#all'>All</a>
+          <a href='#about'>About</a>
+          <ModeButton onClick={onModeClick} />
         </nav>
       </div>
-      <div className="title">
-        THE BLOG
-      </div>
+      <div className='title'>THE BLOG</div>
     </header>
-  );
+  )
 }
