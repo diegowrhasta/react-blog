b9bdeb733348875d9a104352d3f0e0e5bb07d86a
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-01 10:04:49 -0400

    Adds Footer component with working links and styles

--- ds-blog/src/components/Footer.tsx
+++ ds-blog/src/components/Footer.tsx
@@ -1,12 +1,21 @@
 import './Footer.css'
 
+const links = {
+  linkedIn: 'https://www.linkedin.com/in/ds-balderrama/',
+  email: 'mailto:balderrama.quino.diego@gmail.com'
+}
+
 export function Footer () {
+  function onLinkClick (link: string) {
+    window.open(link, '_blank')
+  }
+
   return (
     <div className='links'>
       <ul>
         <li className='static'>© 2024</li>
-        <li>LinkedIn</li>
-        <li>Email</li>
+        <li onClick={() => onLinkClick(links.linkedIn)}>LinkedIn</li>
+        <li onClick={() => onLinkClick(links.email)}>Email</li>
       </ul>
     </div>
   )
