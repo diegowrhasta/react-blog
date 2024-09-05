02a6b9202070eb617170090f6c5d55c019c0a570
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-05 11:01:07 -0400

    Adds initial work for recent blog section layout [INCOMPLETE]

--- ds-blog/src/components/EntrySection/EntrySection.tsx
+++ ds-blog/src/components/EntrySection/EntrySection.tsx
@@ -16,15 +16,39 @@ export function EntrySection ({
 }: EntrySectionProps) {
   const allType = !!isAllType
 
-  const layoutTypeClass = allType ? 'all-container' : 'recent-container'
-  const entries = data.map((entry) => {
-    return <Entry {...entry} />
-  })
+  // const layoutTypeClass = allType ? 'all-container' : 'recent-container'
+  // const entries = data.map(entry => {
+  //   return <Entry {...entry} />
+  // })
+
+  let container = <></>
+  if (!allType) {
+    container = getRecentLayout(data)
+  }
 
   return (
     <>
       <span className='title'>{titleName}</span>
-      <div className={`entry-container ${layoutTypeClass}`}>{entries}</div>
+      {container}
     </>
+  )
+}
+
+function getRecentLayout (entries: EntryInterface[]) {
+  return (
+    <div>
+      <div className='entry-container recent-container'>
+        <div className='first-row'>
+          <Entry {...entries[0]} />
+          <div className='stacked-entries'>
+            <Entry {...entries[1]} />
+            <Entry {...entries[2]} />
+          </div>
+        </div>
+        <div className='last-item'>
+          <Entry {...entries[3]} />
+        </div>
+      </div>
+    </div>
   )
 }
