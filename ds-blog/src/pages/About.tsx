79926dfb3c442059ab9bab9fffcab19e754c094b
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-13 11:09:47 -0400

    Fixes bad practice of <ul> nested inside of a <p>

--- ds-blog/src/pages/About.tsx
+++ ds-blog/src/pages/About.tsx
@@ -34,81 +34,75 @@ export function About () {
       </section>
       <section id='skills'>
         <label>Skills:</label>
-        <p>
-          <ul>
-            <li>
-              Extensive experience in UI porting from design tools such as Figma
-              and Zeplin into Angular and React solutions with Mobile-First and 
-              Desktop-First approaches.
-            </li>
-            <li>
-              Proficiency in development and collaboration tools such as Gitlab,
-              Github, Azure DevOps (CI/CD, SDLC).
-            </li>
-            <li>
-              Excellent design and implementation skills both for architectural
-              structures alongside multi-purpose day-to-day code.
-              (Microservices, Monoliths, Modular Monoliths).
-            </li>
-            <li>
-              Strong communication and collaboration skills, with the ability to
-              work with both small to medium-sized teams.
-            </li>
-            <li>
-              Willingness and experience with leadership roles both from the
-              mentorship perspective but also organization of work and
-              enablement of team members.
-            </li>
-            <li>
-              Vast experience with best practices and tendencies when it comes
-              to Restful APIs and Web Systems.
-            </li>
-            <li>
-              Excellent experience with streaming architectures specifically
-              with Apache Flink, and the ELK Stack.
-            </li>
-          </ul>
-        </p>
+        <ul>
+          <li>
+            Extensive experience in UI porting from design tools such as Figma
+            and Zeplin into Angular and React solutions with Mobile-First and
+            Desktop-First approaches.
+          </li>
+          <li>
+            Proficiency in development and collaboration tools such as Gitlab,
+            Github, Azure DevOps (CI/CD, SDLC).
+          </li>
+          <li>
+            Excellent design and implementation skills both for architectural
+            structures alongside multi-purpose day-to-day code. (Microservices,
+            Monoliths, Modular Monoliths).
+          </li>
+          <li>
+            Strong communication and collaboration skills, with the ability to
+            work with both small to medium-sized teams.
+          </li>
+          <li>
+            Willingness and experience with leadership roles both from the
+            mentorship perspective but also organization of work and enablement
+            of team members.
+          </li>
+          <li>
+            Vast experience with best practices and tendencies when it comes to
+            Restful APIs and Web Systems.
+          </li>
+          <li>
+            Excellent experience with streaming architectures specifically with
+            Apache Flink, and the ELK Stack.
+          </li>
+        </ul>
       </section>
       <section id='experience'>
         <label>Experience:</label>
-        <p>
-          <ul>
-            <li>
-              5 years of experience as a Full Stack Developer, working on a
-              variety of projects for clients in different industries but
-              focused on high transaction sytems for health, banking and
-              financial institutions. (Angular, .NET, React)
-            </li>
-            <li>
-              Experience in conducting performance and benchmarking for high
-              throughput applications.
-            </li>
-            <li>
-              Designed and implemented integration efforts between multiple
-              systems through APIs and different architectural patterns.
-            </li>
-            <li>
-              Implemented Threat Modeling efforts in order to mitigate risks and
-              document design decisions in cross-functional software development
-              teams.
-            </li>
-            <li>
-              2 years of experience with Java, Kotlin and Prime NG techonologies
-              for Web System Development.
-            </li>
-          </ul>
-        </p>
+        <ul>
+          <li>
+            5 years of experience as a Full Stack Developer, working on a
+            variety of projects for clients in different industries but focused
+            on high transaction sytems for health, banking and financial
+            institutions. (Angular, .NET, React)
+          </li>
+          <li>
+            Experience in conducting performance and benchmarking for high
+            throughput applications.
+          </li>
+          <li>
+            Designed and implemented integration efforts between multiple
+            systems through APIs and different architectural patterns.
+          </li>
+          <li>
+            Implemented Threat Modeling efforts in order to mitigate risks and
+            document design decisions in cross-functional software development
+            teams.
+          </li>
+          <li>
+            2 years of experience with Java, Kotlin and Prime NG techonologies
+            for Web System Development.
+          </li>
+        </ul>
       </section>
       <section id='education'>
         <label>Education:</label>
-        <p>
-          <ul>
-            <li>Bachelor's Degree in System Engineering</li>
-            <li>GitHub Foundations Certified</li>
-            <li>Associate's Degree as a Network and Hardware Techinician</li>
-          </ul>
-        </p>
+        <ul>
+          <li>Bachelor's Degree in System Engineering</li>
+          <li>GitHub Foundations Certified</li>
+          <li>Associate's Degree as a Network and Hardware Techinician</li>
+        </ul>
       </section>
     </div>
   )
