1abcdb2461695adcf13caea1e2ce4f9b3eb0ab9c
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-28 08:56:52 -0400

    Adds support for ipad [HALF-WAY, MISSING ALL ENTRIES]

--- ds-blog/src/components/EntrySection/EntrySection.tsx
+++ ds-blog/src/components/EntrySection/EntrySection.tsx
@@ -26,44 +26,106 @@ function EntrySection ({
 
   const container = allType ? getAllLayout(data) : getRecentLayout(data)
 
-  return (
-    <div id={id} className='entry-section-container'>
-      <span className='title'>{titleName}</span>
-      {container}
-      {allType && <Paginator pageNumber={pageNumber!} pageSize={pageSize!} />}
-    </div>
-  )
-}
+  function getAllLayout (entries: EntryInterface[]) {
+    const genericEntries = entries.map(entry => {
+      return <Entry key={entry.id} {...entry} />
+    })
 
-function getAllLayout (entries: EntryInterface[]) {
-  const genericEntries = entries.map(entry => {
-    return <Entry key={entry.id} {...entry} />
-  })
+    return <div className='entry-container all-container'>{genericEntries}</div>
+  }
 
-  return <div className='entry-container all-container'>{genericEntries}</div>
-}
+  function getRecentLayout (entries: EntryInterface[]) {
+    return getRecentLayoutForScreen(entries)
+  }
 
-function getRecentLayout (entries: EntryInterface[]) {
-  return (
-    <div className='entry-container recent-container'>
-      <div className='first-row'>
-        <Entry type={getRecentEntryType(0)} {...entries[0]} />
-        <div className='stacked-entries'>
-          <Entry type={getRecentEntryType(1)} {...entries[1]} />
-          <Entry type={getRecentEntryType(2)} {...entries[2]} />
+  function getRecentLayoutForScreen (entries: EntryInterface[]) {
+    const phoneWindow = window.matchMedia('(max-width): 390px')
+
+    if (phoneWindow.matches) {
+      return (
+        <div className='entry-container recent-desktop-container'>
+          <div className='first-row'>
+            <Entry type={getRecentEntryType(0)} {...entries[0]} />
+            <div className='stacked-entries'>
+              <Entry type={getRecentEntryType(1)} {...entries[1]} />
+              <Entry type={getRecentEntryType(2)} {...entries[2]} />
+            </div>
+          </div>
+          <div className='last-item'>
+            <Entry type={getRecentEntryType(3)} {...entries[3]} />
+          </div>
+        </div>
+      )
+    }
+
+    const ipadWindow = window.matchMedia('(max-width: 834px)')
+
+    if (ipadWindow.matches) {
+      return (
+        <div className='entry-container recent-ipad-container'>
+          <div className='first-row'>
+            <Entry type={getRecentEntryType(0)} {...entries[0]} />
+          </div>
+          <div className='stacked-entries'>
+            <Entry type={getRecentEntryType(1)} {...entries[1]} />
+            <Entry type={getRecentEntryType(2)} {...entries[2]} />
+          </div>
+          <div className='last-item'>
+            <Entry type={getRecentEntryType(3)} {...entries[3]} />
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div className='entry-container recent-desktop-container'>
+        <div className='first-row'>
+          <Entry type={getRecentEntryType(0)} {...entries[0]} />
+          <div className='stacked-entries'>
+            <Entry type={getRecentEntryType(1)} {...entries[1]} />
+            <Entry type={getRecentEntryType(2)} {...entries[2]} />
+          </div>
+        </div>
+        <div className='last-item'>
+          <Entry type={getRecentEntryType(3)} {...entries[3]} />
         </div>
       </div>
-      <div className='last-item'>
-        <Entry type={getRecentEntryType(3)} {...entries[3]} />
-      </div>
-    </div>
-  )
-}
+    )
+  }
 
-function getRecentEntryType (itemIndex: number) {
-  const width = window.innerWidth
+  function getRecentEntryType (itemIndex: number) {
+    const phoneWindow = window.matchMedia('(max-width): 390px')
 
-  if (width >= 1440) {
+    if (phoneWindow.matches) {
+      switch (itemIndex) {
+        case 0:
+          return ENTRY_TYPES.BIG_PICTURE_TOP
+        case 1:
+        case 2:
+          return ENTRY_TYPES.SMALL_PICTURE_LEFT
+        case 3:
+          return ENTRY_TYPES.SMALL_PICTURE_LEFT
+        default:
+          return ENTRY_TYPES.BIG_PICTURE_TOP
+      }
+    }
+
+    const ipadWindow = window.matchMedia('(max-width: 834px)')
+
+    if (ipadWindow.matches) {
+      switch (itemIndex) {
+        case 0:
+          return ENTRY_TYPES.BIG_PICTURE_TOP
+        case 1:
+        case 2:
+          return ENTRY_TYPES.SMALL_PICTURE_LEFT
+        case 3:
+          return ENTRY_TYPES.BIG_PICTURE_TOP
+        default:
+          return ENTRY_TYPES.BIG_PICTURE_TOP
+      }
+    }
+
     switch (itemIndex) {
       case 0:
         return ENTRY_TYPES.MEDIUM_PICTURE_TOP
@@ -77,7 +139,13 @@ function getRecentEntryType (itemIndex: number) {
     }
   }
 
-  return ENTRY_TYPES.BIG_PICTURE_TOP
+  return (
+    <div id={id} className='entry-section-container'>
+      <span className='title'>{titleName}</span>
+      {container}
+      {allType && <Paginator pageNumber={pageNumber!} pageSize={pageSize!} />}
+    </div>
+  )
 }
 
 export { EntrySection }
