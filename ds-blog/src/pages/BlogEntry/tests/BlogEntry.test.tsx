02819672a0c2679fa7dd74875ede2c4ac8d0a907
Diego Balderrama <diegowrhasta@gmail.com>
2024-11-25 10:11:04 -0400

    Adds tests for BlogEntry to test happy path and edge case

---
+++ ds-blog/src/pages/BlogEntry/tests/BlogEntry.test.tsx
@@ -0,0 +1,52 @@
+import { screen, render } from 'testing-library-utils'
+
+import { BlogEntry } from '../BlogEntry'
+import { useBlogDetailStore } from 'src/store'
+import { Route, Routes } from 'react-router-dom'
+
+beforeEach(() => {
+  useBlogDetailStore
+    .getState()
+    .triggerRouting('70e2c51a-f70f-4df5-9cb8-bc4b88260888')
+})
+
+test('page has accesible and navigable properties', () => {
+  render(<BlogEntry />)
+
+  const sidebarRegion = screen.getByRole('region', {
+    name: /Recent entries sidebar/i
+  })
+  const postContentRegion = screen.getByRole('region', {
+    name: /Post Content/i
+  })
+  expect(sidebarRegion).toBeInTheDocument()
+  expect(postContentRegion).toBeInTheDocument()
+})
+
+test('uninitialized store fallsback to route param', async () => {
+  useBlogDetailStore.getState().reset()
+  const mockedParam = '70e2c51a-f70f-4df5-9cb8-bc4b88260888'
+
+  render(
+    <Routes>
+      <Route path='/entry/:id' element={<BlogEntry />}></Route>
+    </Routes>,
+    {
+      routerProps: { initialEntries: [`/entry/${mockedParam}`] }
+    }
+  )
+
+  const sidebarRegion = screen.getByRole('region', {
+    name: /Recent entries sidebar/i
+  })
+  const postContentRegion = screen.getByRole('region', {
+    name: /Post Content/i
+  })
+  expect(sidebarRegion).toBeInTheDocument()
+  expect(postContentRegion).toBeInTheDocument()
+
+  const loadedPost = await screen.findByRole('article', {
+    name: /Software, Pragmatism and Epistemology/i
+  })
+  expect(loadedPost).toBeInTheDocument()
+})
